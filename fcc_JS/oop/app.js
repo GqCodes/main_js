680bc4d03bb28b91a2ae185d80335877bc8202ec
Chris Vargas <gqcodes@gmail.com>
2022-09-03 16:33:07 -0400

    add oop coding challenge

--- fcc_JS/oop/app.js
+++ fcc_JS/oop/app.js
@@ -71,3 +71,10 @@ for (let property in canary) {
     ownProps.push(property);
   }
 }
+
+// Use Prototype Properties to Reduce Duplicate Code
+function Dog(name) {
+  this.name = name;
+}
+Dog.prototype.numLegs = 4;
+let beagle = new Dog('Snoopy');
