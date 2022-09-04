9e6616a08396e612b34343ac5c49895c2219e1cd
Chris Vargas <gqcodes@gmail.com>
2022-09-04 16:33:26 -0400

    add oop coding challenge

--- fcc_JS/oop/app.js
+++ fcc_JS/oop/app.js
@@ -78,3 +78,23 @@ function Dog(name) {
 }
 Dog.prototype.numLegs = 4;
 let beagle = new Dog('Snoopy');
+
+// iterate over all properties
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog('Snoopy');
+
+let ownProps = [];
+let prototypeProps = [];
+
+for (let property in beagle) {
+  if (beagle.hasOwnProperty(property)) {
+    ownProps.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+}
