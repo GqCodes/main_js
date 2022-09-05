e68978109caf612dfd99b343cadefd2eb308be44
Chris Vargas <gqcodes@gmail.com>
2022-09-05 16:33:49 -0400

    add oop coding challenge

--- fcc_JS/oop/app.js
+++ fcc_JS/oop/app.js
@@ -98,3 +98,16 @@ for (let property in beagle) {
     prototypeProps.push(property);
   }
 }
+
+// understand constructor property
+function Dog(name) {
+  this.name = name;
+}
+// Only change code below this line
+function joinDogFraternity(candidate) {
+  if (candidate.constructor === Dog) {
+    return true;
+  } else {
+    return false;
+  }
+}
