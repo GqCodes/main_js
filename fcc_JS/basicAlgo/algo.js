b8a9dd54c16c0dcdb840fdfd2ce013d52b827ad9
Chris Vargas <gqcodes@gmail.com>
2022-11-10 14:55:23 -0500

    add algo coding challenge

--- fcc_JS/basicAlgo/algo.js
+++ fcc_JS/basicAlgo/algo.js
@@ -29,3 +29,19 @@ function factorialize(num) {
 }
 
 factorialize(5);
+
+// find longest word in string
+function findLongestWordLength(str) {
+  let longestWord = '';
+  let words = str.split(' ');
+
+  for (let i = 0; i < words.length; i++) {
+    let individualWord = words[i];
+    if (individualWord.length > longestWord.length) {
+      longestWord = individualWord;
+    }
+  }
+  return longestWord.length;
+}
+
+findLongestWordLength('The quick brown fox jumped over the lazy dog');
