04d847e2e24d79c69f27a83bbbb855e00e29cef4
Chris Vargas <gqcodes@gmail.com>
2022-11-13 14:57:12 -0500

    add algo coding challenge

--- fcc_JS/basicAlgo/algo.js
+++ fcc_JS/basicAlgo/algo.js
@@ -76,3 +76,14 @@ function confirmEnding(str, target) {
 }
 
 confirmEnding('Bastian', 'n');
+
+// repeat a string
+function repeatStringNumTimes(str, num) {
+  let finalString = '';
+  for (let i = 0; i < num; i++) {
+    finalString += str;
+  }
+  return finalString;
+}
+
+repeatStringNumTimes('abc', 3);
