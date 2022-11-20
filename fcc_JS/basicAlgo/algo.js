e9cc9b61d2567255945118119a92c0e2ed1fafd8
Chris Vargas <gqcodes@gmail.com>
2022-11-20 15:00:33 -0500

    add algo coding challenge

--- fcc_JS/basicAlgo/algo.js
+++ fcc_JS/basicAlgo/algo.js
@@ -112,3 +112,14 @@ function booWho(bool) {
 }
 
 booWho(null);
+
+// title case a sentence
+function titleCase(str) {
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map((val) => val.replace(val.charAt(0), val.charAt(0).toUpperCase()))
+    .join(' ');
+}
+
+titleCase("I'm a little tea pot");
