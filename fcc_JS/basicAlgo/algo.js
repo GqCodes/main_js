e3120d991652f3423de7dfbf64971c791b7336db
Chris Vargas <gqcodes@gmail.com>
2022-11-24 15:03:28 -0500

    add algo coding challenge

--- fcc_JS/basicAlgo/algo.js
+++ fcc_JS/basicAlgo/algo.js
@@ -151,3 +151,18 @@ function customSort(a, b) {
 }
 
 getIndexToIns([40, 60], 50);
+
+// mutations
+function mutation(arr) {
+  const [str1, str2] = arr.map((s) => s.toLowerCase());
+
+  for (let i = 0; i < str2.length; i++) {
+    const letter = str2[i];
+    if (str1.indexOf(letter) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+mutation(['hello', 'hey']);
