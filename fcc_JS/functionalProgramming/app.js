a4f2a0157980bd84ee8113d373dfa98da37ae419
Chris Vargas <gqcodes@gmail.com>
2023-01-01 15:25:04 -0500

    add algo coding challenge

--- fcc_JS/functionalProgramming/app.js
+++ fcc_JS/functionalProgramming/app.js
@@ -638,3 +638,16 @@ function alphabeticalOrder(arr) {
 }
 
 alphabeticalOrder(['a', 'd', 'c', 'a', 'z', 'g']);
+
+// RETURN SORTED ARRAY WITHOUT MUTATION
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  const newArr = [].concat(arr);
+
+  return newArr.sort(compareFunction);
+  // Only change code above this line
+}
+
+nonMutatingSort(globalArray);
