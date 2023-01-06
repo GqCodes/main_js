73ba4b3cc8299ae9be93b57369a2a9cd1475b428
Chris Vargas <gqcodes@gmail.com>
2023-01-06 15:26:39 -0500

    add algo coding challenge

--- fcc_JS/functionalProgramming/app.js
+++ fcc_JS/functionalProgramming/app.js
@@ -686,3 +686,16 @@ function checkPositive(arr) {
 }
 
 checkPositive([1, 2, 3, -4, 5]);
+
+// CURRYING AND PARTIAL APPLICATION
+function add(x) {
+  // Only change code below this line
+  return function (y) {
+    return function (z) {
+      return x + y + z;
+    };
+  };
+  // Only change code above this line
+}
+
+add(10)(20)(30);
