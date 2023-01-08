38032686811a3867f83635149dc9f38820768c9e
Chris Vargas <gqcodes@gmail.com>
2023-01-08 15:29:26 -0500

    add oop coding challenge

--- fcc_JS/oop/app.js
+++ fcc_JS/oop/app.js
@@ -3,3 +3,12 @@ let dog = {
   name: 'pablo',
   numLegs: 3,
 };
+
+// dot notation to access properties
+let dog = {
+  name: 'Spot',
+  numLegs: 4,
+};
+
+console.log(dog.name);
+console.log(dog.numLegs);
