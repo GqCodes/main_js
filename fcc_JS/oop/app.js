4c6c18dda007ef4a28f9b17fbbcb1277f768d278
Chris Vargas <gqcodes@gmail.com>
2023-03-02 10:50:21 -0500

    add oop coding challenge

--- fcc_JS/oop/app.js
+++ fcc_JS/oop/app.js
@@ -1,18 +1,3 @@
-// create an object
-let dog = {
-  name: 'pablo',
-  numLegs: 3,
-};
-
-// dot notation to access properties
-let dog = {
-  name: 'Spot',
-  numLegs: 4,
-};
-
-console.log(dog.name);
-console.log(dog.numLegs);
-
 // create a method on an object
 let dog = {
   name: 'Spot',
--- fcc_JS/oop/main.js
+++ fcc_JS/oop/main.js
@@ -1,166 +1,3 @@
-// create an object
-let dog = {
-  name: "pablo",
-  numLegs: 3,
-};
-
-// dot notation to access properties
-let dog = {
-  name: "Spot",
-  numLegs: 4,
-};
-
-console.log(dog.name);
-console.log(dog.numLegs);
-
-// create a method on an object
-let dog = {
-  name: "Spot",
-  numLegs: 4,
-  sayLegs: function () {
-    return `This dog has ${this.numLegs} legs.`;
-  },
-};
-
-dog.sayLegs();
-
-// define a constructor function
-function Dog() {
-  this.name = "Rupert";
-  this.color = "brown";
-  this.numLegs = 4;
-}
-
-// use constructor to create a (NEW) object
-function Dog() {
-  this.name = "Rupert";
-  this.color = "brown";
-  this.numLegs = 4;
-}
-
-let hound = new Dog();
-
-// extending contructors to receive arguments
-function Dog(name, color) {
-  this.name = name;
-  this.color = color;
-  this.numLegs = 4;
-}
-
-let terrier = new Dog("grumpy", "white");
-
-// Verify an Object's Constructor with instanceof
-function House(numBedrooms) {
-  this.numBedrooms = numBedrooms;
-}
-
-let myHouse = new House(6);
-myHouse instanceof House;
-
-// understand own properties
-function Bird(name) {
-  this.name = name;
-  this.numLegs = 2;
-}
-
-let canary = new Bird("Tweety");
-let ownProps = [];
-
-for (let property in canary) {
-  if (canary.hasOwnProperty(property)) {
-    ownProps.push(property);
-  }
-}
-
-// Use Prototype Properties to Reduce Duplicate Code
-function Dog(name) {
-  this.name = name;
-}
-Dog.prototype.numLegs = 4;
-let beagle = new Dog("Snoopy");
-
-// iterate over all properties
-function Dog(name) {
-  this.name = name;
-}
-
-Dog.prototype.numLegs = 4;
-
-let beagle = new Dog("Snoopy");
-
-let ownProps = [];
-let prototypeProps = [];
-
-for (let property in beagle) {
-  if (beagle.hasOwnProperty(property)) {
-    ownProps.push(property);
-  } else {
-    prototypeProps.push(property);
-  }
-}
-
-// understand constructor property
-function Dog(name) {
-  this.name = name;
-}
-// Only change code below this line
-function joinDogFraternity(candidate) {
-  if (candidate.constructor === Dog) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// change prototype to a new object
-function Dog(name) {
-  this.name = name;
-}
-
-Dog.prototype = {
-  numLegs: 2,
-  eat: function () {
-    console.log("nom nom nom");
-  },
-  describe: function () {
-    console.log("My name is " + this.name);
-  },
-};
-
-// setting constructor property when changing the prototype
-function Dog(name) {
-  this.name = name;
-}
-
-// Only change code below this line
-Dog.prototype = {
-  constructor: Dog,
-  numLegs: 4,
-  eat: function () {
-    console.log("nom nom nom");
-  },
-  describe: function () {
-    console.log("My name is " + this.name);
-  },
-};
-
-// understand where an object's prototype comes from
-function Dog(name) {
-  this.name = name;
-}
-
-let beagle = new Dog("Snoopy");
-Dog.prototype.isPrototypeOf(beagle);
-
-// understand the prototype chain
-function Dog(name) {
-  this.name = name;
-}
-
-let beagle = new Dog("Snoopy");
-
-Dog.prototype.isPrototypeOf(beagle); // yields true
-
 // Fix the code below so that it evaluates to true
 Object.prototype.isPrototypeOf(Dog.prototype);
 
@@ -186,7 +23,7 @@ function Animal() {}
 Animal.prototype = {
   constructor: Animal,
   eat: function () {
-    console.log("nom nom nom");
+    console.log('nom nom nom');
   },
 };
 
@@ -196,7 +33,7 @@ function Animal() {}
 Animal.prototype = {
   constructor: Animal,
   eat: function () {
-    console.log("nom nom nom");
+    console.log('nom nom nom');
   },
 };
 
@@ -209,7 +46,7 @@ function Animal() {}
 Animal.prototype = {
   constructor: Animal,
   eat: function () {
-    console.log("nom nom nom");
+    console.log('nom nom nom');
   },
 };
 
@@ -234,7 +71,7 @@ let beagle = new Dog();
 // add methods after inheritance
 function Animal() {}
 Animal.prototype.eat = function () {
-  console.log("nom nom nom");
+  console.log('nom nom nom');
 };
 
 function Dog() {}
@@ -242,7 +79,7 @@ function Dog() {}
 Dog.prototype = Object.create(Animal.prototype);
 Dog.prototype.constructor = Dog;
 Dog.prototype.bark = function () {
-  console.log("Woof!");
+  console.log('Woof!');
 };
 
 let beagle = new Dog();
@@ -251,7 +88,7 @@ let beagle = new Dog();
 function Bird() {}
 
 Bird.prototype.fly = function () {
-  return "I am flying!";
+  return 'I am flying!';
 };
 
 function Penguin() {}
@@ -259,20 +96,20 @@ Penguin.prototype = Object.create(Bird.prototype);
 Penguin.prototype.constructor = Penguin;
 
 Penguin.prototype.fly = function () {
-  return "Alas, this is a flightless bird.";
+  return 'Alas, this is a flightless bird.';
 };
 let penguin = new Penguin();
 console.log(penguin.fly());
 
 // Use a Mixin to Add Common Behavior Between Unrelated Objects
 let bird = {
-  name: "Donald",
+  name: 'Donald',
   numLegs: 2,
 };
 
 let boat = {
-  name: "Warrior",
-  type: "race-boat",
+  name: 'Warrior',
+  type: 'race-boat',
 };
 
 let glideMixin = function (obj) {
@@ -293,7 +130,7 @@ function Bird() {
 
 // Immediately invoked function expression (IIFE)
 (function () {
-  console.log("A cozy nest is ready");
+  console.log('A cozy nest is ready');
 })();
 
 // use IIFE to create a module
@@ -307,8 +144,23 @@ let funModule = (function () {
     },
     singMixin: function (obj) {
       obj.sing = function () {
-        console.log("Singing to an awesome tune");
+        console.log('Singing to an awesome tune');
       };
     },
   };
 })();
+
+// create an object
+let dog = {
+  name: 'pablo',
+  numLegs: 3,
+};
+
+// dot notation to access properties
+let dog = {
+  name: 'Spot',
+  numLegs: 4,
+};
+
+console.log(dog.name);
+console.log(dog.numLegs);
