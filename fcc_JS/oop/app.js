c1cfa1ccf560230a1eb62a177e19be451c419a11
Chris Vargas <gqcodes@gmail.com>
2023-03-10 09:04:19 -0500

    add oop coding challenge

--- fcc_JS/oop/app.js
+++ fcc_JS/oop/app.js
@@ -130,3 +130,16 @@ Animal.prototype = {
     console.log('nom nom nom');
   },
 };
+
+// inherit behaviors from a Supertype
+function Animal() {}
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function () {
+    console.log('nom nom nom');
+  },
+};
+
+let duck = Object.create(Animal.prototype);
+let beagle = Object.create(Animal.prototype);
