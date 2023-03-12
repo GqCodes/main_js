5a2211daa97ce2f3f8520ad79a057114f4b8b73a
Chris Vargas <gqcodes@gmail.com>
2023-03-12 12:52:14 -0400

    add oop coding challenge

--- fcc_JS/oop/app.js
+++ fcc_JS/oop/app.js
@@ -157,3 +157,17 @@ Animal.prototype = {
 function Dog() {}
 Dog.prototype = Object.create(Animal.prototype);
 let beagle = new Dog();
+
+// reset inherited constructor properties
+function Animal() {}
+function Bird() {}
+function Dog() {}
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+let duck = new Bird();
+let beagle = new Dog();
