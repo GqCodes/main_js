6692be77377f6b3dd79502405efebf5d22375f19
Chris Vargas <gqcodes@gmail.com>
2023-03-27 09:09:36 -0400

    add oop challenge

--- fcc_JS/oop/app.js
+++ fcc_JS/oop/app.js
@@ -236,3 +236,20 @@ function Bird() {
 (function () {
   console.log('A cozy nest is ready');
 })();
+
+// use IIFE to create a module
+
+let funModule = (function () {
+  return {
+    isCuteMixin: function (obj) {
+      obj.isCute = function () {
+        return true;
+      };
+    },
+    singMixin: function (obj) {
+      obj.sing = function () {
+        console.log('Singing to an awesome tune');
+      };
+    },
+  };
+})();
